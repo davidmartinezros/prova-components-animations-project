8e0f0f3e1c9e26d27fb5467cc230b631fd88f8f0
David Martinez Ros <davidnezan@gmail.com>
2020-04-08 11:09:10 +0200

    push of version 0.0.5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { AppComponentAnimationTemplateModule } from 'app-component-animation-template';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppComponentAnimationTemplateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
